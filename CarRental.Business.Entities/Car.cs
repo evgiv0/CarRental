a70a9b10419054e562061133d1a79c349fcf5055
Ivaneev Evgeny <zhe956@gmail.com>
2018-01-15 22:56:51 +0300

    First commit

---
+++ CarRental.Business.Entities/Car.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Car
+    {
+        [DataMember]
+        public int CarId { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public decimal RentalPrice { get; set; }
+
+        [DataMember]
+        public bool CurrentlyRented { get; set; }
+    }
+}
---
+++ CarRental.Client.Entities/Car.cs
@@ -0,0 +1,45 @@
+﻿namespace CarRental.Client.Entities
+{
+
+    public class Car
+    {
+        private int _carId;
+        private readonly string _description;
+        private readonly string _color;
+        private readonly int _year;
+        private readonly decimal _rentalPrice;
+        private readonly bool _currentlyRented;
+
+        private string Description
+        {
+            get { return _description; }
+        }
+
+        private string Color
+        {
+            get { return _color; }
+        }
+
+        private int Year
+        {
+            get { return _year; }
+        }
+
+        private decimal RentalPrice
+        {
+            get { return _rentalPrice; }
+        }
+
+        private bool CurrentlyRented
+        {
+            get { return _currentlyRented; }
+        }
+
+        public int CarId
+        {
+            get => _carId;
+
+            set => _carId = value;
+        }
+    }
+}
